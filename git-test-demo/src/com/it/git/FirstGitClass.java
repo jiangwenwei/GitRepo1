40cb8707a8fa46c27ad0511805a2b61e27ab036f
WenweiJiang <jiangwenwei01@126.com>
2020-02-23 17:40:48 +0800

    把本地工程添加到git

---
+++ git-test-demo/src/com/it/git/FirstGitClass.java
@@ -0,0 +1,14 @@
+package com.it.git;
+
+public class FirstGitClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+    }
+}
